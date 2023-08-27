2ebefdd79f178d3fd4676d797a89c24cca1b7289
nikitkachan <nikyc777@gmail.com>
2023-08-27 16:31:35 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,27 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
 
-const test = test;
+function renderImgEl (arr, container) {
+    const markup = arr.map(
+        (img) => `<li class="gallery__item">
+   <a class="gallery__link" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+   </a>
+</li>`  
+    )
+    .join("");
+
+  container.insertAdjacentHTML("beforeend", markup);
+};
+
+renderImgEl(galleryItems, galleryEl);
+
+const gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: "250" });
+
+galleryEl.style.listStyle = "none";
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("iframe");
+
+const player = new Player(iframe);
+
+const onPlay = function (e) {
+    localStorage.setItem('video-current-time', JSON.stringify(e.seconds));
+    };
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('video-current-time'));
