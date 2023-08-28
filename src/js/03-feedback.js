f2f59d1fc7c2aa57a1922b73073bd6286013d8fd
nikitkachan <nikyc777@gmail.com>
2023-08-28 10:07:58 +0300

    Готово

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector(".feedback-form");
+const textarea = document.querySelector(".feedback-form textarea");
+const inputEl = document.querySelector(".feedback-form input");
+
+formEl.addEventListener("submit", onFormSubmit);
+formEl.addEventListener("input", throttle(onTextInput, 500));
+
+const dataObj = {};
+const savedObj = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+savedData();
+
+function onTextInput(evt) {
+    dataObj[evt.target.name] = evt.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataObj));
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+     console.log(savedObj);
+    localStorage.removeItem('feedback-form-state')
+    evt.currentTarget.reset();
+};
+
+function savedData() {
+    if (savedObj) {
+        inputEl.value = savedObj.email;
+        textarea.value = savedObj.message;
+    };
+};
